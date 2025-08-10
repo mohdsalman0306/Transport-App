bc95a63fd39b0b93ca24a9624a888d9f53bc443b
Salman <mohd.salman0306@gmail.com>
2025-08-10 18:47:57 +0400

    Used env variable for the client

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:5001/api", // change as per backend
+	baseURL: import.meta.env.VITE_SERVER_API_URL, // change as per backend
 });
 
 api.interceptors.request.use((config) => {
